d22afc24544e53e0d66480de4ab7496931079357
MY5T3R10 <mr.bannicin@gmail.com>
2023-12-01 22:21:17 +0300

    Revert "Merge pull request #1 from MY5T3RI0/bulatBranch"
    This reverts commit ccb81a2f8a61e586c0d36f32e63c7753989bfc0e, reversing
    changes made to 459458dc8d21f105233dd85ea6c774f4aedb45bc.

--- src/string_funcs/s21_memcmp.c
+++ src/string_funcs/s21_memcmp.c
@@ -1,10 +1,12 @@
 #include "s21_string.h"
 
 int s21_memcmp(const void* str1, const void* str2, size_t n) {
-  int compare_index = 0;
-  for (size_t i = 0; i < n && compare_index == 0; i++) {
-	compare_index = *((char*)str1 + i) - *((char*)str2 + i);
-  }
+  const unsigned char* str_1 = str1;
+  const unsigned char* str_2 = str2;
+  int res = 0;
 
-  return compare_index;
+  for (size_t i = 0; i < n && !res; i++)
+    if (*(str_1 + i) != *(str_2 + i)) res = *(str_1 + i) - *(str_2 + i);
+
+  return res;
 }
